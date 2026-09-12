181c55941ee7336041b4b2d8ea44d68203aca4b4
Mohitubba <119030323+Mohitubba@users.noreply.github.com>
2026-09-12 22:32:12 +0530

    first

---
+++ src/main/java/in/coderarmy/githubActionDemo/HelloController.java
@@ -0,0 +1,18 @@
+package in.coderarmy.githubActionDemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Coder Army!";
+    }
+
+    @GetMapping("/bye")
+    public String bye() {
+        return "Bye Bye!";
+    }
+}
